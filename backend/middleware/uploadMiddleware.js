a800111c9e0dd49d7f1cbee0866b1312f8dd187b
KrishnaPatel <kp06krish@gmail.com>
2025-12-14 00:43:36 +0530

    added upload middleware

---
+++ backend/middleware/uploadMiddleware.js
@@ -0,0 +1,24 @@
+import multer from 'multer'
+
+const storage = multer.diskStorage({
+    destination:( req, file, cb) =>{
+        cb(null,"uploads/")
+    },
+    filename:( req, file, cb) =>{
+        cb(null, `${Date.now()}-${file.originalname}`)
+    },
+});
+
+//file filter
+const filefilter = (req, file , cb) =>{
+    const allowedTypes =["iamges/jpeg", "iamges/png", "iamges/png",];
+    if (allowedTypes.includes(file.mimetype)){
+        cb (null, true)
+    }
+    else{
+        cb(new Error(" only .jpeg , .png or .jpg are allowed formats", false))
+    }
+}
+
+const upload = multer({storage, fileFilter})
+export default upload;
