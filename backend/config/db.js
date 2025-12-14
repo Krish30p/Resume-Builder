7a5148d0fdb95e7b16ad15b8b12676d27adfcd28
KrishnaPatel <kp06krish@gmail.com>
2025-12-14 21:19:57 +0530

    fix: update MongoDB connection string to use environment variable and correct user model import

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://kp07krish:Resume1234567890@cluster0.mpqiu1r.mongodb.net/Resume')
+    await mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('DB Connected'))
 
 }
