d378f4170f31f94c634af5dadceb21042ece2300
KrishnaPatel <kp06krish@gmail.com>
2025-12-19 23:54:27 +0530

    error changes

--- frontend/src/context/userContext.jsx
+++ frontend/src/context/userContext.jsx
@@ -1,4 +1,6 @@
 import React, {  createContext, useEffect, useState } from "react";
+import axiosInstance from "../utils/axiosInstance";
+import { API_PATHS } from "../utils/apiPath";
 
 export const UserContext = createContext();
 
@@ -16,12 +18,38 @@ const UserProvider = ({children}) =>{
         }
         const fetchUser = async()=>{
             try {
-                const response = await ax
+                const response = await axiosInstance.get(API_PATHS.AUTH.GET_PROFILE)
+                setUser(response.data)
             } catch (error) {
+                console.error("User not authenticated", error)
+                clearUser()
                 
             }
-        }
-    })
+            finally{
+                setLoading(false)
+            }
+        };
+        fetchUser();
+
+    },[]);
+
+    const updateUser = (userData) =>{
+        setUser(userData)
+        localStorage.setItem('token', userData.token)
+        setLoading(false)
+    }
+
+    const clearUser = () =>{
+        setUser(null)
+        localStorage.removeItem('token')
+    }
+
+    return (
+        <UserContext.Provider value={{user, loading , updateUser, clearUser}}>
+            {children}
+        </UserContext.Provider>
+    )
 
 }
+export default UserProvider;
 
